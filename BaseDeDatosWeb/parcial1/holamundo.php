638a465ba1d2061ef7a98b3cde8a837cfcc51b0a
Zamora Sánchez Luis Alberto <159496697+Luistachos@users.noreply.github.com>
2024-08-30 18:45:14 -0700

    Hola Mundo 30/08/24

---
+++ BaseDeDatosWeb/parcial1/holamundo.php
@@ -0,0 +1,17 @@
+<?php
+$nombre = "Zamora Sanchez Luis Alberto";
+$numero = 5;
+$numero2 = "5";
+
+$total = $numero + $numero2;
+
+echo "Total =" . $total;
+
+echo "<h1> HOLA MUNDO </h1>";
+
+for($i=0; $i<10; $i++){
+    echo $i . "<br>";
+}
+
+echo "<h3 style='color: violet;'>Zamora Sanchez Luis Alberto</h3>";
+?>
